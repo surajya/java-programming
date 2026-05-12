c7c28edbb2c6f6828e2852caa9ade6e6cd1acd96
surajvns1571999 <surajvns1571999@gmail.com>
2026-05-12 07:27:21 +0530

    understand main thread in java world

---
+++ Multithreading/src/com/multithread/Section02.java
@@ -0,0 +1,10 @@
+package com.multithread;
+
+public class Section02 {
+
+	public static void main(String[] args) {
+		System.out.println("Main thread started");
+		System.out.println(Thread.currentThread().getName());
+	}
+
+}
